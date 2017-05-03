ff46d59269d8ec09028023f1b1293b7a767aebf5
Rushabh179 <modi.rushabh179@gmail.com>
2017-05-03 12:03:45 +0530

    Bug fix in railfence

--- Railfance.py
+++ Railfance.py
@@ -1,6 +1,6 @@
 s=(str(input('enter string..')))
 l=(int(input('enter level..')))-1
 lev=[""]*(l+1);i=0
-for x in s:
-	lev[abs(l-s.index(x)%(l*2))]+=x
-print("cypher text :- ",''.join(r for r in lev))
+for x in range(len(s)):
+	lev[abs(l-x%(l*2))]+=s[x]
+print("cypher text :- ",''.join(r for r in lev[::-1]))
